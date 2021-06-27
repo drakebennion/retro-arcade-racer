79733104d8c822df32d3ae7cbb9d5bd45ec1b136
drakobian <drake.bennion@gmail.com>
2021-06-27 11:44:46 -0600

    feat: start new project using game_engine

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use olc_rust_game_engine::{Color, ConsoleGameEngine, Painter, Result, Rules};
+
+struct RacerRules {
+
+}
+
+impl RacerRules {
+    fn new() -> Self {
+        RacerRules{}
+    }
+}
+
+impl Rules for RacerRules {
+    fn on_user_create(&mut self, painter: &mut Painter) {
+
+    }
+
+    fn on_user_update(&mut self, painter: &mut Painter, elapsed_time: f64) {
+
+    }
+}
+
+fn main() -> Result<()> {
+    let rules = RacerRules::new();
+    let mut game = ConsoleGameEngine::new(100, 160, rules);
+    game.construct_console()?;
+    game.start()?;
+
+    Ok(())
+}
