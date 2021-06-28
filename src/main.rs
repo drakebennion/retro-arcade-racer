d330cba8b31af8ae1dd6271b91859b88f1bcb034
drakobian <drake.bennion@gmail.com>
2021-06-27 20:47:48 -0600

    feat: make scenery respond to movement

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use olc_rust_game_engine::{Color, ConsoleGameEngine, Utils, Result, Rules};
 
 struct RacerRules {
-
+    car_distance: f64,
+    car_position: f64
 }
 
 impl RacerRules {
     fn new() -> Self {
-        RacerRules{}
+        RacerRules{
+            car_distance : 0.0,
+            car_position : 0.0
+        }
     }
 }
 
@@ -16,10 +20,16 @@ impl Rules for RacerRules {
     }
 
     fn on_user_update(&mut self, utils: &mut Utils, elapsed_time: f64) {
+        if utils.keys.contains(&38) {
+            self.car_distance += 500.0 * elapsed_time;
+        }
+
+        // draw road, clip, and grass
         for y in 0..(utils.height / 2) {
             for x in 0..utils.width {
+                let perspective = y as f64 / (utils.height as f64 / 2.0);
                 let middle = 0.5;
-                let mut road_width = 0.6;
+                let mut road_width = 0.1 + perspective * 0.8;
                 let clip_width = road_width * 0.15;
                 road_width /= 2.0;
 
@@ -29,16 +39,24 @@ impl Rules for RacerRules {
                 let right_clip = (middle + road_width) * utils.width as f64;
 
                 let y_flip = (utils.height / 2) + y;
+
+                let grass_arg: f64 = 20.0 * (1.0 - perspective).powf(3.0) + self.car_distance * 0.1;
+                let grass_color = if grass_arg.sin() > 0.0 { Color::Green } else { Color::DarkGreen };
                 match x {
-                    x if x < left_grass as usize => utils.draw(x, y_flip, '█', Color::Green),
+                    x if x < left_grass as usize => utils.draw(x, y_flip, '█', grass_color),
                     x if x >= left_grass as usize && x < left_clip as usize => utils.draw(x, y_flip, '█', Color::Red),
                     x if x >= left_clip as usize && x < right_clip as usize => utils.draw(x, y_flip, '█', Color::DarkGrey),
                     x if x >= right_clip as usize && x < right_grass as usize => utils.draw(x, y_flip, '█', Color::Red),
-                    x if x >= right_grass as usize => utils.draw(x, y_flip, '█', Color::Green),
+                    x if x >= right_grass as usize => utils.draw(x, y_flip, '█', grass_color),
                     _ => ()
                 } 
             }
         }
+
+        // draw car
+        let car_pos = utils.width / 2 + ( utils.width * self.car_position as usize / 2) - 4;
+        // todo: make this car more car-y
+        utils.fill(car_pos, 80, car_pos + 4, 84, '#', Color::Black);
     }
 }
 
