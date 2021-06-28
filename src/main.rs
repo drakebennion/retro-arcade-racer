822cb5bb58947aa80ca2bb347e2a7b5217259b3b
drakobian <drake.bennion@gmail.com>
2021-06-27 18:13:22 -0600

    feat: draw road start

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,33 @@ impl RacerRules {
 
 impl Rules for RacerRules {
     fn on_user_create(&mut self, utils: &mut Utils) {
-
+        utils.fill(0, 0, utils.width, utils.height, ' ', Color::Black);
     }
 
     fn on_user_update(&mut self, utils: &mut Utils, elapsed_time: f64) {
+        for y in 0..(utils.height / 2) {
+            for x in 0..utils.width {
+                let middle = 0.5;
+                let mut road_width = 0.6;
+                let clip_width = road_width * 0.15;
+                road_width /= 2.0;
 
+                let left_grass = (middle - road_width - clip_width) * utils.width as f64;
+                let right_grass = (middle + road_width + clip_width) * utils.width as f64;
+                let left_clip = (middle - road_width) * utils.width as f64;
+                let right_clip = (middle + road_width) * utils.width as f64;
+
+                let y_flip = (utils.height / 2) + y;
+                match x {
+                    x if x < left_grass as usize => utils.draw(x, y_flip, '█', Color::Green),
+                    x if x >= left_grass as usize && x < left_clip as usize => utils.draw(x, y_flip, '█', Color::Red),
+                    x if x >= left_clip as usize && x < right_clip as usize => utils.draw(x, y_flip, '█', Color::DarkGrey),
+                    x if x >= right_clip as usize && x < right_grass as usize => utils.draw(x, y_flip, '█', Color::Red),
+                    x if x >= right_grass as usize => utils.draw(x, y_flip, '█', Color::Green),
+                    _ => ()
+                } 
+            }
+        }
     }
 }
 
